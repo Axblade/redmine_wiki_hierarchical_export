56a215c36e092bdca1b4670571e4f70c922de7bf
Axblade <eugene.sypachev@gmail.com>
2012-04-09 21:37:15 +0400

    red mine version requirement

--- init.rb
+++ init.rb
@@ -7,8 +7,10 @@ Redmine::Plugin.register :redmine_wiki_hierarchical_export do
   author 'Eugene Sypachev'
   description 'You can export wiki child pages by one click with this plugin'
   version '0.1'
-  url 'http://example.com/path/to/plugin'
+  url 'https://github.com/Axblade/redmine_wiki_hierarchical_export'
   author_url 'https://github.com/Axblade'
+
+  requires_redmine :version_or_higher => '1.3.0'
 
   Dispatcher.to_prepare :redmine_wiki_hierarchical_export do
     # nothing here
